5542ec6baf68c3686acd96fa4d4626e9ea9a5580
Samuel Inverso <samuel.inverso@gmail.com>
2014-04-22 14:07:32 -0400

    genbank_to_csv.py now can take a csv to filter entries included.

--- src/genbank_to_csv.py
+++ src/genbank_to_csv.py
@@ -1,5 +1,11 @@
-#! python
+#! env python
 """Converts a genbank file into a table loadable to the Database
+    
+   For usage type:
+   python genbank_file_to_csv.py -h 
+   
+   # from src directory
+   python genbank_to_csv.py ../tests/gi_U00096.gbk ../tests -f ../tests/blattner_filter.csv
 
    Author: Samuel A. Inverso, Wyss Institute, 2014-04-16
 """
@@ -11,8 +17,24 @@ from argparse import RawTextHelpFormatter
 from Bio import SeqIO 
 from Bio import Entrez
 import pandas as pd
+import csv
 
-def getFromKeyList(lst,key,sep=":",firstOnly=False):
+def load_filter_file(filter_file):
+    """Reads in a csv filter file
+
+        filter_file: file name, or None
+        returns a DataFrame with the csv data or an empty DataFrame if 
+            filter_file is none
+    """
+    df = pd.DataFrame()
+    # if
+    if filter_file:
+        with open(filter_file,'rb') as csvfile:
+            df = pd.read_csv(csvfile)
+    return df
+
+
+def get_from_key_list(lst,key,sep=":",firstOnly=False):
     """ Parases a list of strings that have a deliminator
     """
     result = []
@@ -31,9 +53,12 @@ def getFromKeyList(lst,key,sep=":",firstOnly=False):
 
     return result
 
-def genbank_dataframe_to_database_csv(df, output_dir, sep=','):
+
+def genbank_dataframe_to_database_csv(df, output_dir, sep=',',index=False):
     """Takes a genbank datafram and converts it into csv's
         That can be loaded into the database
+
+        index : boolean, wrete row names (index), default False
     """
     gene_columns = ['gene_ID', 'Alternate_Name', 'Sequence' ]
     protein_columns =  ['pp_ID', 'Amino_Acid_Sequence', 'gene_ID']
@@ -46,11 +71,12 @@ def genbank_dataframe_to_database_csv(df, output_dir, sep=','):
     
     # Gene
     output_path = os.path.join(output_dir, gene_file_name) 
-    df[gene_columns].to_csv(output_path,sep)
+    df[gene_columns].to_csv(output_path,sep,index=index)
 
     # protein
     output_path = os.path.join(output_dir, protein_file_name)
-    df[protein_columns].to_csv(output_path,sep)
+    df[protein_columns].to_csv(output_path,sep,index=index)
+
 
 def genbank_feature_to_dict(feature, record):
     """Take a genbank feature and put it into a dictionary with
@@ -68,11 +94,11 @@ def genbank_feature_to_dict(feature, record):
         result['Coding_Indicies'] = str(feature.location)
 
         # Protein
-        result['UniProt_ID'] = getFromKeyList( 
+        result['UniProt_ID'] = get_from_key_list( 
                           feature.qualifiers['db_xref'],"UniProtKB/Swiss-Prot",
                           firstOnly=True )
 
-        result['pp_ID'] = getFromKeyList( 
+        result['pp_ID'] = get_from_key_list( 
                           feature.qualifiers['db_xref'],"UniProtKB/Swiss-Prot",
                           firstOnly=True )
 
@@ -91,24 +117,56 @@ def genbank_feature_to_dict(feature, record):
 
     return result 
 
-def genbank_record_to_dataframe(record):
+
+def genbank_record_to_dataframe(record,dfFilter=None):
     """Converts a genbank Bio:Record to a dictionary that
         can be then converted into a CSV
 
         Uses the database mapping.
     """
-
     rows = []
     for feature in record.features:
         row = genbank_feature_to_dict(feature,record)
         if row:
             rows.append(row)
 
-    return pd.DataFrame(rows)
+    df = pd.DataFrame(rows)
+    
+    df = filter_dataframe(df,dfFilter)
 
-def genbank_file_to_record(filename):
+    return df
+
+
+def filter_dataframe(df,dfFilter):
+    """ Match columns between df and dfFilter. If the values in 
+        df appear in dfFilter then we keep that row.
+
+        Only columns that appear in both are used for filtering
+        i.e. extra columns in dfFilter that are not in df are ignored.
+    """
+    result = df
+    if dfFilter is not None and not dfFilter.empty:
+        # for some reason using the dataframe as a filter is not working
+        # so we are going to loop trough the columns
+        col_names = list(dfFilter.columns.values)
+        df_col_names = list(df.columns.values)
+        # only loop through the columns that are in df
+        mask = [True] * len(df.index)
+        for col in  col_names:
+            if col in df:
+                mask &= df[col].isin(dfFilter[col])
+    
+        result = df[mask]
+
+    return  result
+
+
+def genbank_file_to_record(filename,dfFilter=None):
+    """ Reads a genbank file and returns a BioPython record
+    """
     record = SeqIO.read(filename,"genbank")
-    return genbank_record_to_dataframe(record)
+    return genbank_record_to_dataframe(record,dfFilter)
+
 
 def main(argv):
     """Main entry point of program.
@@ -124,17 +182,33 @@ def main(argv):
     parser.add_argument('output_dir', metavar='output_dir', action='store',
         help='Output directory to place csvs into.')
 
+    parser.add_argument('-f', metavar='filter', action='store', dest='filter',
+        help='A CSV file with column header the name of a tag to filter'
+            ' on and rows with values to keep.'
+            ' The row header should be the name that will appear in the final'
+            ' database table. For example, to filter on blattner number or an'
+            ' ecoli genbank file, make a CSV file with one column'
+            '     Alternate_Name\n'
+            '     b0002\n'
+            '     b0003\n'
+            '\n'
+            'python genbank_file_to_csv gi_U00096.gbk . -f blattner_filter.csv\n'
+            'Limits to entries with blattner numbes b0002 and b0003.' )
+
     args = parser.parse_args()
     genbank_file = args.gb
     output_dir = args.output_dir
+    filter_file = args.filter
 
+
+    # get the filter file if it exists
+    dfFilter = load_filter_file(filter_file)
 
     # Convert the records to a dataframe
-    df = genbank_file_to_record(genbank_file)
+    df = genbank_file_to_record(genbank_file, dfFilter)
 
     # turn the dataframe into a series of tables
     genbank_dataframe_to_database_csv(df,output_dir)
-
 
 # Call the main method if we're run as script
 if __name__ == "__main__":
